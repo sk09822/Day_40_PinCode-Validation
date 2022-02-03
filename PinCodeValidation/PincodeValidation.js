50fb31fbb59dc000531dd2e9177daacda51eb744
sk09822 <kanadesandipan.virtuoso@gmail.com>
2022-02-03 19:23:25 +0530

    Added : PinCode Regex that validates 6 digit Pincode

--- PinCodeValidation/PincodeValidation.js
+++ PinCodeValidation/PincodeValidation.js
@@ -1,1 +1,16 @@
-console.log("Welcome to PincodeValidation");
+const pincodeRegex = RegExp("^[0-9]{3}\\s{0,1}[0-9]{3}$");
+
+function validatePincode(pincode){
+    if(pincodeRegex.test(pincode))
+        console.log(pincode + " : Valid Pin Code !!");
+    else
+        throw pincode + " : PINCODE is Invalid !";
+}
+
+try{
+    validatePincode("400088");
+    validatePincode("400 088");
+}
+catch(e){
+    console.error(e);
+}
